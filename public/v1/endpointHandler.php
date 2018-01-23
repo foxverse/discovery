b1437961d437999ffc574b8bee2b69dfa1412851
ninjafox <antherynx.studio@mail.com>
2018-01-23 02:59:35 -0500

    OKAY LMAO

--- public/v1/endpointHandler.php
+++ public/v1/endpointHandler.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../../discovery.php');
+require_once(__DIR__ . DIRECTORY_SEPARATOR . '../../discovery.php');
 
 $dom = new DOMDocument();
 $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><result/>');
